c67c9c6cf6644c2bfb72639c38fb316b9c714328
Derrick L. Mayiku <40387583+DeroMal@users.noreply.github.com>
2023-01-06 23:31:57 +0300

    Update 101-safely_get_value.py

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """module element length"""
+from typing import Any, Mapping, Union
 
 
-def safely_get_value(dct, key, default = None):
+def safely_get_value(dct: Mapping, key: Any, default: Union[Any, None] = None) -> Union[Any, None]:
     """safely get value"""
     if key in dct:
         return dct[key]
